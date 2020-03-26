df1cd06eaae1015a41a2bae0184a9c777e2cf4d5
Prakash <165936@NTTDATA.COM>
2020-03-26 20:33:37 +0530

    Updated the code for PDF headless download

--- TestPdfDownload/Class1.cs
+++ TestPdfDownload/Class1.cs
@@ -9,46 +9,92 @@ using System.Linq;
 //using System.Collections.Generic;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Remote;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace TestPdfDownload
 {
     public class PdfDownload
     {
-        public static IWebDriver driver;
+        public static IWebDriver Driver { get; private set; }
+        //public static ChromeDriver driver = null;
+
+
         [SetUp]
         public void startBrowser()
         {
-            var tsTimeout = new TimeSpan(0, 5, 0);
+            System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"C:/UmaPrakash/chromedriverwin32/chromedriver.exe");
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless", "--window-size=1920,1080");
 
+            var driverService = ChromeDriverService.CreateDefaultService("C:\\UmaPrakash\\chromedriverwin32", "chromedriver.exe");
+            Driver = new ChromeDriver(driverService, chromeOptions);
+                     
+
+            Task.Run(() => AllowHeadlessDownload(driverService));
+
+            /*var tsTimeout = new TimeSpan(0, 5, 0);
+            
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddUserProfilePreference("download.default_directory", "C:\\UmaPrakash\\TestDownloads");
+            chromeOptions.AddArguments("headless");
+            //chromeOptions.AddUserProfilePreference("download.default_directory", "C:\\UmaPrakash\\TestDownloads");
             chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
             chromeOptions.AddUserProfilePreference("download.directory_upgrade", true);
             chromeOptions.AddUserProfilePreference("plugins.plugins_disabled", "Chrome PDF Viewer");
             chromeOptions.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
+            chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
+
+            var paramList = new Dictionary<string, object>();
+            paramList.Add("behavior", "allow");
+            paramList.Add("downloadPath", "C:\\UmaPrakash\\TestDownloads");
+            
 
             driver = new ChromeDriver("C:\\UmaPrakash\\chromedriver_win32\\", chromeOptions, tsTimeout);
-
+            driver.ExecuteChromeCommand("Page.setDownloadBehavior", paramList); */
 
         }
+
+
 
         [Test]
         public void LaunchUrl()
         {
-            driver.Url = "https://www.guru99.com/selenium-tutorial-pdf.html";
-            driver.Manage().Window.Maximize();
+            System.Console.WriteLine("Into the Test");
+            
+            Driver.Url = "http://www.seleniumhq.org/download/";
+            Driver.Manage().Window.Maximize();
             System.Threading.Thread.Sleep(20000);
+            Driver.FindElement(By.LinkText("32 bit Windows IE")).Click();
 
-            String title = driver.Title;
-            Assert.AreEqual(title, "Selenium Tutorial PDF");
+            System.Threading.Thread.Sleep(5000);            
 
-            driver.FindElement(By.XPath("//p[3]//a[1]//img[1]")).Click();
         }
+
+        static async Task AllowHeadlessDownload(ChromeDriverService driverService)
+        {
+            var jsonContent = new JObject(
+                new JProperty("cmd", "Page.setDownloadBehavior"),
+                new JProperty("params",
+                new JObject(new JObject(
+                    new JProperty("behavior", "allow"),
+                    new JProperty("downloadPath", @"C:\UmaPrakash\TestDownloads")))));
+            var content = new StringContent(jsonContent.ToString(), Encoding.UTF8, "application/json");
+            var sessionIdProperty = typeof(ChromeDriver).GetProperty("SessionId");
+            var sessionId = sessionIdProperty.GetValue(Driver, null) as SessionId;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = driverService.ServiceUrl;
+                var result = await client.PostAsync("session/" + sessionId.ToString() + "/chromium/send_command", content);
+                var resultContent = await result.Content.ReadAsStringAsync();
+            }
+        }
+       
        
         [TearDown]
         public void closeBrowser()
         {
-            // System.Threading.Thread.Sleep(10000);
+             System.Threading.Thread.Sleep(10000);
             // driver.Close();
         }
     }
